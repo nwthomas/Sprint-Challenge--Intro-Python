14df37b0a16bb3a8226cb5bc3a5eba9059d18eac
Nathan Thomas <nwthomas@me.com>
2019-09-06 08:22:40 -0700

    Implement last two list comprehensions

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -1,6 +1,6 @@
 # The following list comprehension exercises will make use of the
 # defined Human class.
-import math
+from math import sqrt
 
 
 class Human:
@@ -65,10 +65,10 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [Human(h.name.upper(), h.age + 5) for h in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
-h = []
+h = [sqrt(h.age) for h in humans]
 print(h)
